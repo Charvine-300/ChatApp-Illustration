6b7d6180dc5e31bdb8abe2141bfbb4f9a9d0c000
LordEra <marvinezenwekwe@gmail.com>
2021-06-29 13:58:11 +0100

    Phone fix

--- chatapp.js
+++ chatapp.js
@@ -8,11 +8,13 @@ if (document.body.clientWidth > 1000) {
     
     else if (document.body.clientWidth < 375) {
         stud.style.width = `${document.body.clientWidth - 125}px`;
-    }    
+    }  
+    
+    if (document.body.clientWidth > 375) {
+        stunner.style.marginLeft = `${document.body.clientWidth - 335}px`;
+    }
 }
-if (document.body.clientWidth > 375) {
-    stunner.style.marginLeft = `${document.body.clientWidth - 335}px`;
-}
+
 
 
 
