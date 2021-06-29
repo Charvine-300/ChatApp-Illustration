55b106a53f3f5fae4b082232345345ada1c9062d
LordEra <marvinezenwekwe@gmail.com>
2021-06-29 13:46:12 +0100

    Chnaged margins and added JS File

---
+++ chatapp.js
@@ -0,0 +1,17 @@
+var stud = document.getElementById("sidechick");
+var stunner = document.getElementById("phone");
+
+if (document.body.clientWidth > 375) {
+    stud.style.width = `${document.body.clientWidth - 125}px`;
+}
+
+else if (document.body.clientWidth < 375) {
+    stud.style.width = `${document.body.clientWidth - 125}px`;
+}
+
+if (document.body.clientWidth > 375) {
+    stunner.style.marginLeft = `${document.body.clientWidth - 335}px`;
+}
+
+
+
